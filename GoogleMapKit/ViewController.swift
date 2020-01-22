ae889379a58c548631b209a1352e21aeeffe57e8
Артем Холод <artsiom.kholodok@gmail.com>
2020-01-22 18:36:43 +0300

    Initial Commit

---
+++ GoogleMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapKit
+//
+//  Created by Macbook on 22.01.2020.
+//  Copyright © 2020 EPLAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
